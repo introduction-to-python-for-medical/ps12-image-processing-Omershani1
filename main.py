8cfe95d7da532d74c552fe72e25a9489c91791d1
Omershani1 <shani.omer1@gmail.com>
2025-01-29 15:31:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.io import imread
+from skimage.color import rgb2gray
+from PIL import Image
 
+def add_stripes(image_array, stripe_width):
+    height, width = image_array.shape
+    striped_image = image_array.copy()
+
+    for i in range(0, width, 2 * stripe_width):
+        striped_image[:, i:i + stripe_width] = 0  # פסים שחורים
+
+    return striped_image
+
+image_array = imread('costarica.jpg')
+
+if image_array.ndim == 3:
+    image_array = rgb2gray(image_array)
+
+image_array = (image_array * 255).astype(np.uint8)
+
+striped_image = add_stripes(image_array, stripe_width=20)
+
+image_pil = Image.fromarray(striped_image, mode='L')
+image_pil.save('striped_costarica.jpeg')
+
+plt.imshow(striped_image, cmap='gray')
+plt.title('Striped Costa Rica')
+plt.axis('off')
+plt.show()
